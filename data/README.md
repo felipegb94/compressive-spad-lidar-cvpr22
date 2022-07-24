b1eed3f558c579dc9d9e1cdae192b2e9ccbc6265
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2022-07-24 10:44:32 -0700

    add initial download_data script

---
+++ data/README.md
@@ -0,0 +1,9 @@
+# Data
+
+To download the data please run `scripts/download_data.py` from the top-level folder:
+
+```
+python scripts/download_data.py
+```
+
+The data will be download inside the `./data` folder. If you want to change the folder where the data is downloaded edit the parameter `data_base_dirpath` inside `io_dirpaths.json`.
---
+++ scripts/download_data.py
@@ -0,0 +1,38 @@
+'''
+    This script downloads the data used in the cvpr paper
+    Make sure to run this script from the top-level folder of the repository
+    i.e.,
+        python scripts/download_data.py
+'''
+
+#### Standard Library Imports
+import zipfile
+import os
+import sys
+sys.path.append('.')
+sys.path.append('..')
+
+#### Library imports
+import gdown
+
+#### Local imports
+from research_utils.io_ops import load_json
+
+if __name__=='__main__':
+
+    ## 
+    io_dirpaths = load_json('io_dirpaths.json')
+    data_base_dir = io_dirpaths['data_base_dirpath']
+    print(data_base_dir)
+
+    ## Output dir where to store the dataset (leaving default should work)
+    os.makedirs(data_base_dir, exist_ok=True)
+    
+    ## dataset folder ID
+    dataset_dir = data_base_dir
+
+    ## Get the url 
+    gdrive_dataset_folder_url = "https://drive.google.com/drive/folders/1vJsZxGgXxh1s-wLIpSIcFg0hPx8pDJea"
+
+    zip_fpaths = gdown.download_folder(url=gdrive_dataset_folder_url, output=dataset_dir, quiet=False)
+
